19ddd96a6c79501139cd4ad0b9450e33f39c1fb1
Fabio <fabioalbertocataldo@gmail.com>
2025-10-06 14:43:20 -0300

    fix: angular.json, postcss file, main.ts, app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CocktailFilterComponent } from './components/cocktail-filter/cocktail-filter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CocktailFilterComponent],
+  imports: [CocktailFilterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
